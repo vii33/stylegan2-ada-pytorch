a2b552c84a5bdfd9d66e753abe559456e6f787f5
vii33 <github.20.losvochos@spamgourmet.com>
2021-04-10 14:54:05 +0200

    Create calculate_gamma.py

---
+++ utils/calculate_gamma.py
@@ -0,0 +1,26 @@
+import click
+
+@click.command()
+#@click.pass_context
+
+@click.option('--img_width', help='Train images width', required=True, type=click.INT)
+@click.option('--img_height', help='Train images height', required=True, type=click.INT)
+@click.option('--mini_batch_size', help='Train minibatch', required=True, type=click.INT)
+
+def calculate_gamma (img_width:int, img_height:int, mini_batch_size:int):
+  '''
+  Calculates gamma search range for given img dimensions and minibatch size
+  '''
+  
+  gamma_0 = 0.0002 * img_width * img_height / mini_batch_size
+  print(f"gamma_0 is {gamma_0}. Do a grid search in the range: [ {gamma_0/5} | {gamma_0*5} ]")
+
+
+
+#----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+  calculate_gamma()
+
+#----------------------------------------------------------------------------
+
